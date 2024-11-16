cd76bca78ae41d589e372544523eb78c36f88aee
matt handler <matt.handler@gmail.com>
2024-11-16 00:57:19 -0500

    update webpack to not include new stuff in production

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,55 +1,63 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const path = require('path');
 
-module.exports = {
-  entry: './src/index.tsx',
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              plugins: [
-                'babel-plugin-styled-components',
-                process.env.NODE_ENV !== 'production' && 'react-refresh/babel',
-              ].filter(Boolean),
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    entry: './src/index.tsx',
+    mode: isDevelopment ? 'development' : 'production',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                plugins: isDevelopment
+                  ? [
+                      isDevelopment && 'babel-plugin-styled-components',
+                      isDevelopment && 'react-refresh/babel',
+                    ]
+                  : [],
+              },
             },
-          },
-          'ts-loader',
-        ],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-    new ReactRefreshWebpackPlugin(),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+            'ts-loader',
+          ],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
     },
-    compress: true,
-    port: 9000,
-    historyApiFallback: true,
-    hot: true,
-  },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle.[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean),
+    devServer: isDevelopment
+      ? {
+          static: {
+            directory: path.join(__dirname, 'dist'),
+          },
+          compress: true,
+          port: 9000,
+          historyApiFallback: true,
+          hot: true,
+        }
+      : undefined,
+  };
 };
